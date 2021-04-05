9e07833d28105362a8c01365d5032eb0c15ddb44
Josh Bruflodt <norsebru@gmail.com>
2021-04-05 16:07:49 +0200

    Render formatted text first

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,22 @@ import "reflect-metadata";
 import './app/app';
 
 // components (FYI, order of these represents order in which they will appear... For now)
+import { HtmlClipboardRepresentation } from "./components/content-representation/html-representation/html-clipboard-representation.component";
 import { CabinetLoaderComponent } from "./components/cabinet-loader/cabinet-loader.component";
 import { ImageContentRepresentationComponent } from "./components/content-representation/image-content/image-content-representation.component";
 import { ImageTextRepresentationComponent } from "./components/content-representation/image-text/image-text-representation.component";
 import { JsonContentRepresentationComponent } from "./components/content-representation/json/json-content-representation.component";
 import { TextContentRepresentationComponent } from "./components/content-representation/text/text-content-representation.component";
 import { PastaDrawerComponent } from "./components/pasta-drawer-app/pasta-drawer.component";
-import { HtmlClipboardRepresentation } from "./components/content-representation/html-representation/html-clipboard-representation.component";
 // import { WebpageRepresentationComponent } from "./components/content-representation/webpage/webpage-representation.component";
 
+HtmlClipboardRepresentation;
 CabinetLoaderComponent;
 PastaDrawerComponent;
 TextContentRepresentationComponent;
 JsonContentRepresentationComponent;
 ImageContentRepresentationComponent;
 ImageTextRepresentationComponent;
-HtmlClipboardRepresentation;
+
 // WebpageRepresentationComponent; // WIP
 
