485751639c7eee4bbe564b522cbf7256f383c21a
Josh Bruflodt <norsebru@gmail.com>
2021-06-20 11:09:33 +0200

    Update service-worker.js

--- www/service-worker.js
+++ www/service-worker.js
@@ -22,32 +22,8 @@ self.addEventListener('fetch', (e) => {
   );
 });
 
-// self.addEventListener("fetch", (event) => {
-//   const url = new URL(event.request.url);
-  
-//   if (event.request.method === "POST" && url.pathname === "/bookmark") {
-//     event.respondWith(
-//       (async () => {
-//         const formData = await event.request.formData();
-//         const link = formData.get("link") || "";
-        
-//         return new Response('Bookmark saved: ' + link);
-
-//         // const responseUrl = await saveBookmark(link);
-//         // return Response.redirect(responseUrl, 303);
-//       })()
-//     );
-//   } else {
-//     event.respondWith(
-//       caches.match(event.request).then((response) => {
-//         return response || fetch(event.request);
-//       })
-//     );
-//   }
-// });
-
 // Update a service worker
-const cacheWhitelist = ['pasta-drawer-v1.0.1'];
+const cacheWhitelist = [CACHE_NAME];
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
