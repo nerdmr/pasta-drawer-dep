bccff48aa300b0af6e59d16a66da61c557abed63
Josh Bruflodt <norsebru@gmail.com>
2022-01-08 16:29:58 +0100

    Re-version app, add fonts to cache

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,9 +1,17 @@
 // Installing service worker
-const CACHE_NAME = 'pasta-drawer-v1.0.3';
+const CACHE_NAME = 'pasta-drawer-v1.1.0';
 
 /* Add relative URL of all the static content you want to store in
  * cache storage (this will help us use our app offline)*/
-let resourcesToCache = ['/', '/bundle.js', '/index.html', '/img/192.png', '/img/512.png'];
+let resourcesToCache = [
+    '/',
+    '/bundle.js',
+    '/index.html',
+    '/img/192.png',
+    '/img/512.png',
+    'https://fonts.gstatic.com/s/materialicons/v83/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+    'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Roboto+Mono:wght@300&family=Roboto:ital,wght@0,100;0,300;1,100;1,300&family=Zen+Antique&display=swap'
+];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
